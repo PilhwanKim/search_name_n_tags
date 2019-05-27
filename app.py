2c79e2f26b2b2e968c0cacb521927b3d659bf11b
pilhwanKim <pilhwankim03@gmail.com>
2019-05-28 05:34:47 +0900

    예외처리 입력

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request, jsonify
+from flask import request, jsonify, abort
 
 app = Flask(__name__)
 
@@ -13,6 +13,17 @@ def hello_world():
 def get_companies():
     company_name = request.args.get('name', '')
     company_tag = request.args.get('tag', '')
+
+    # 파라메터 유무 체크
+    if not company_tag and not company_name:
+        return "name or tag parameter is empty.", 400
+
+    # 파라메터 길이 체크 1
+    if company_name and len(company_name) > 255:
+        return "name parameter is too long.", 400
+    if company_tag and len(company_tag) > 255:
+        return "tag parameter value is too long.", 400
+
     companies = [
         {
             "id": 2,
@@ -30,7 +41,6 @@ def get_companies():
 
 @app.route('/companies/<int:company_id>/tags/<tag_name>', methods=['POST'])
 def add_tags(company_id, tag_name):
-    # return '{} {}'.format(company_id, tag_name)
     return jsonify({
         'company_id': company_id,
         'tag_name': tag_name
@@ -39,11 +49,15 @@ def add_tags(company_id, tag_name):
 
 @app.route('/companies/<int:company_id>/tags/<tag_name>', methods=['DELETE'])
 def delete_tags(company_id, tag_name):
-    # return '{} {}'.format(company_id, tag_name)
     return jsonify({
         'company_id': company_id,
         'tag_name': tag_name
     })
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'This page does not exist', 404
 
 
 if __name__ == '__main__':
