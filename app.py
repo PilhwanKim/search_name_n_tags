280240de3c1c05464f09a0a688ce60cb4a3642ba
Leon Kim <pilhwankim03@gmail.com>
2019-05-28 10:07:32 +0900

    pytest 도입 및 api unittest

--- app.py
+++ app.py
@@ -1,64 +1,66 @@
 from flask import Flask
-from flask import request, jsonify, abort
-
-app = Flask(__name__)
+from flask import request, jsonify
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello! This is search names and tags Project!'
+def create_app():
+    app = Flask(__name__)
 
+    @app.route('/')
+    def main():
+        return 'Hello! This is search names and tags Project!'
 
-@app.route('/companies', methods=['GET'])
-def get_companies():
-    company_name = request.args.get('name', '')
-    company_tag = request.args.get('tag', '')
+    @app.route('/companies', methods=['GET'])
+    def get_companies():
+        company_name = request.args.get('name', '')
+        company_tag = request.args.get('tag', '')
 
-    # 파라메터 유무 체크
-    if not company_tag and not company_name:
-        return "name or tag parameter is empty.", 400
+        # 파라메터 유무 체크
+        if not company_tag and not company_name:
+            return "name or tag parameter is empty.", 400
 
-    # 파라메터 길이 체크 1
-    if company_name and len(company_name) > 255:
-        return "name parameter is too long.", 400
-    if company_tag and len(company_tag) > 255:
-        return "tag parameter value is too long.", 400
+        # 파라메터 길이 체크 1
+        if company_name and len(company_name) > 255:
+            return "name parameter is too long.", 400
+        if company_tag and len(company_tag) > 255:
+            return "tag parameter value is too long.", 400
 
-    companies = [
-        {
-            "id": 2,
-            "name": "이상한마케팅",
-            "tags": ["태그_25", "태그_6", "태그_14", "태그_9"]
+        companies = [
+            {
+                "id": 2,
+                "name": "이상한마케팅",
+                "tags": ["태그_25", "태그_6", "태그_14", "태그_9"]
+            }
+        ]
+        response_data = {
+            'search': {
+                'name': company_name,
+                'tag': company_tag,
+            },
+            'companies': companies,
         }
-    ]
-    response_data = {
-        'name': company_name,
-        'tag': company_tag,
-        'companies': companies,
-    }
-    return jsonify(response_data)
+        return jsonify(response_data)
 
+    @app.route('/companies/<int:company_id>/tags/<tag_name>', methods=['POST'])
+    def add_tags(company_id, tag_name):
+        return jsonify({
+            'company_id': company_id,
+            'tag_name': tag_name
+        })
 
-@app.route('/companies/<int:company_id>/tags/<tag_name>', methods=['POST'])
-def add_tags(company_id, tag_name):
-    return jsonify({
-        'company_id': company_id,
-        'tag_name': tag_name
-    })
+    @app.route('/companies/<int:company_id>/tags/<tag_name>', methods=['DELETE'])
+    def delete_tags(company_id, tag_name):
+        return jsonify({
+            'company_id': company_id,
+            'tag_name': tag_name
+        })
 
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return 'This page does not exist', 404
 
-@app.route('/companies/<int:company_id>/tags/<tag_name>', methods=['DELETE'])
-def delete_tags(company_id, tag_name):
-    return jsonify({
-        'company_id': company_id,
-        'tag_name': tag_name
-    })
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return 'This page does not exist', 404
+    return app
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
---
+++ test_api.py
@@ -0,0 +1,62 @@
+import pytest
+from flask import url_for
+
+
+def test_main_url(client):
+    result = client.get(url_for("main"))
+    assert result.status_code == 200
+    assert result.data == b"Hello! This is search names and tags Project!"
+
+
+def test_get_companies_by_name(client):
+    result = client.get(url_for("get_companies", name="코리아"))
+    assert result.status_code == 200
+    assert result.headers.get('Content-Type') == 'application/json'
+    assert result.json == {
+        'companies': [
+            {'id': 2, 'name': '이상한마케팅', 'tags': ['태그_25', '태그_6', '태그_14', '태그_9']}
+        ],
+        'search': {
+            'name': '코리아',
+            'tag': '',
+        }
+    }
+
+
+def test_get_companies_by_tag(client):
+    result = client.get(url_for("get_companies", tag="태그_4"))
+    assert result.status_code == 200
+    assert result.headers.get('Content-Type') == 'application/json'
+    assert result.json == {
+        'companies': [
+            {'id': 2, 'name': '이상한마케팅', 'tags': ['태그_25', '태그_6', '태그_14', '태그_9']}
+        ],
+        'search': {
+            'name': '',
+            'tag': '태그_4',
+        }
+    }
+
+
+def test_add_tags(client):
+    company_id = 1
+    tag_name = "태그_4"
+    result = client.post(url_for("add_tags", company_id=company_id, tag_name=tag_name))
+    assert result.status_code == 200
+    assert result.headers.get('Content-Type') == 'application/json'
+    assert result.json == {
+            'company_id': company_id,
+            'tag_name': tag_name
+        }
+
+
+def test_delete_tags(client):
+    company_id = 1
+    tag_name = "태그_4"
+    result = client.delete(url_for("delete_tags", company_id=company_id, tag_name=tag_name))
+    assert result.status_code == 200
+    assert result.headers.get('Content-Type') == 'application/json'
+    assert result.json == {
+            'company_id': company_id,
+            'tag_name': tag_name
+        }
